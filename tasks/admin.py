3d63dc46dd104e37e014f9a2f7bad5a48f270c33
Marat-Shainurov <m_shainurov@mail.ru>
2023-08-31 16:28:40 +0300

    updates the Task model admin interface

--- tasks/admin.py
+++ tasks/admin.py
@@ -5,6 +5,7 @@ from tasks.models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'parent_task', 'executor', 'owner', 'status', 'deadline', 'is_active')
+    list_display = ('id', 'title', 'parent_task', 'executor', 'owner', 'status', 'deadline', 'is_active',
+                    'in_progres_from', 'execution_date')
     search_fields = ('title', 'executor', 'owner', 'status')
     filter_field = ('executor', 'owner', 'status', 'deadline', 'is_active')
