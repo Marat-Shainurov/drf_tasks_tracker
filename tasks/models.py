e573c9c2b8bb26d22848a65063bb30ac76fe724a
Marat-Shainurov <m_shainurov@mail.ru>
2023-08-31 16:30:35 +0300

    adds in_progres_from execution_date fields to Task model, updates the status field validation

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from rest_framework.exceptions import ValidationError
 
@@ -14,24 +16,34 @@ class Task(models.Model):
     parent_task = models.ForeignKey('self', verbose_name='parent_task', related_name='child_tasks',
                                     on_delete=models.CASCADE, **NULLABLE)
     executor = models.ForeignKey(Employee, verbose_name='task_executor', on_delete=models.RESTRICT,
-                                 related_name='executor_tasks')
+                                 related_name='executor_tasks', **NULLABLE)
     owner = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, verbose_name='task_owner', **NULLABLE,
                               related_name='task_owner')
     status = models.CharField(max_length=11, verbose_name='task_status', choices=STATUS_CHOICES, default='created')
+    in_progres_from = models.DateTimeField(verbose_name='in_progres_from', **NULLABLE)
     deadline = models.DateTimeField(verbose_name='task_deadline')
-    is_active = models.BooleanField(verbose_name='task_activity_flag', default=True)
+    execution_date = models.DateTimeField(verbose_name='execution_date', **NULLABLE)
+    is_active = models.BooleanField(verbose_name='is_active', default=True)
 
     def __str__(self):
         return self.title
 
     def clean_status(self):
         if not self.is_active:
-            msg = 'You cannot change the status value of an inactive task. Activate the task or create a new one'
+            msg = "You cannot change the 'status' field value of a inactive task."
             raise ValidationError(msg)
         elif self.status == 'done' and not check_tasks_chain_status(self):
-            raise ValidationError(f'Not all the child tasks of the {self} task are done yet!')
+            raise ValidationError(f"Not all the child tasks of the '{self}' task are done yet!")
         elif self.status == 'done':
-            self.is_active = False
+            if self.in_progres_from:
+                self.is_active = False
+                self.execution_date = datetime.datetime.now()
+            else:
+                raise ValidationError("Only 'in_progress' tasks can be assigned with 'done'!")
+        elif self.status == 'in_progress' and not self.executor:
+            raise ValidationError(f"You can't assign the 'in_progress' status to a task without an executor!")
+        elif self.status == 'in_progress':
+            self.in_progres_from = datetime.datetime.now()
 
     def save(self, *args, **kwargs):
         self.clean_status()
