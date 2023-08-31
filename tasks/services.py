04d58c1b230e523dd420bc680a7c324edf894703
Marat-Shainurov <m_shainurov@mail.ru>
2023-08-31 16:31:46 +0300

    adds the check_tasks_chain_status() service function, that checks the tasks chain (if all the tasks 'done' or not)

--- tasks/services.py
+++ tasks/services.py
@@ -2,7 +2,10 @@ from rest_framework.exceptions import ValidationError
 
 
 def check_tasks_chain_status(task):
-    child_tasks = task.child_tasks.all()
+    try:
+        child_tasks = task.child_tasks.all()
+    except ValueError:
+        raise ValidationError("A task can't be 'done' while its creation!")
     if child_tasks.exists():
         all_child_tasks_done = all(check_tasks_chain_status(child_task) for child_task in child_tasks)
         if all_child_tasks_done and all(child_task.status == 'done' for child_task in child_tasks):
