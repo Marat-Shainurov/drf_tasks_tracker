d347c06da409f72c09159bc1ea0943e84541ccaa
Marat-Shainurov <m_shainurov@mail.ru>
2023-09-01 12:15:21 +0300

    fixes the password hashing in CustomUserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from users.models import CustomUser
@@ -6,4 +7,7 @@ from users.models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'email', 'phone')
+        fields = ('id', 'email', 'phone', 'password')
+
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
