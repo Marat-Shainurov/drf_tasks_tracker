3950e31ae76a9cc97d4eac3a270d6a7e4e1acab7
Marat-Shainurov <m_shainurov@mail.ru>
2023-09-02 12:37:13 +0300

    sets env variables, add .env, env.sample files

--- config/settings.py
+++ config/settings.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -22,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6t0dh4_c^tmglz*1hxz*qjpj+yh*k@v=)$kgg-rn-_ncb-7wo9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -82,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tasks_tracker',
         'USER': 'postgres',
-        'PASSWORD': 'Benzokolon1'
+        'PASSWORD': os.getenv('DATABASES_PASSWORD')
     }
 }
 
